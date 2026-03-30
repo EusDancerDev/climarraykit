76c3add5ca231df2de77b4c072efaed7c0a82106
EusDancerDev <jagabantxo@gmail.com>
2026-03-30 23:28:53 +0200

    Align conda post-link pip pins with pyproject
    - Set `filewise>=3.13.0` next to `pygenutils>=16.4.0` and `paramlib>=3.4.10` so conda environments match the PyPI dependency story after the xarray_utils move to climarraykit.

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Interdependent PyPI packages (aligned with pyproject.toml [project.dependencies])
 "${PREFIX}/bin/pip" install -v \
-  "filewise>=3.12.0" \
-  "pygenutils>=16.3.2" \
+  "filewise>=3.13.0" \
+  "pygenutils>=16.4.0" \
   "paramlib>=3.4.10"
